1f5ace095aaca479f352a697bf37bed6e9e115e8
Tymoteusz Nachtman <tynach99@gmail.com>
2020-10-27 16:34:50 +0100

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+// Wyświetlanie różnych wartości w zależności od liczby punktów pobranych z wyniku
+const url_string = "http://btake.me/ap/result?score=57"; //skorzystamy z automatycznego pobierania później (window.location.href)
+const url = new URL(url_string);
+const score = url.searchParams.get("score");
+
+const get_score_text = (score) => {
+    if(score != null){        
+        if(score <= 10){
+            return 'Masz dużo pracy nad profilem, ale nie przejmuj się - dostaniesz porady.';
+        }else if(score <= 20){
+            return 'Jest jeszcze wiele do poprawy, ale spokojnie - pomożemy Ci!';
+        }else if(score < 50){
+            return 'Twój profil można jeszcze dopracować.';
+        }else if(score <= 80){
+            return 'Twój profil wydaje się być dobrze uzupełniony, jednak wciąż można poprawić kilka rzeczy.';
+        }else if(score < 100){
+            return 'Brawo! Twój profil wygląda świetnie!';
+        }else{
+            return 'Rewelacja! Twój profil jest idealny!';
+        }
+    }
+}
+
+$('#diagram').attr('data-circles-value', score);
+$('#punktacja').text(score + " / 100 pkt.");
+$('#opis').text(get_score_text(score));
