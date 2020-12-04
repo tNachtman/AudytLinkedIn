40ea844d23d4898e88edf552bc4fb574df111c72
Tymoteusz Nachtman <tynach99@gmail.com>
2020-12-04 16:04:19 +0100

    AutoScreen 0-100

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,7 +25,7 @@ const get_score_text = (score) => {
 
 let test = "";
 const makeScreenshot = () => {
-    html2canvas(document.getElementById("screenshot"), {scale: 2, allowTaint: true, useCORS: true}).then(canvas => {
+    html2canvas(document.getElementById("screenshot"), {scale: 1, allowTaint: true, useCORS: true}).then(canvas => {
         canvas.id = "canvasID";
         let main = document.getElementById("main");
         while (main.firstChild){ main.removeChild(main.firstChild);}
@@ -39,5 +39,17 @@ $('#opis').text(get_score_text(score));
 $('#a-make').on('click', makeScreenshot);
 document.getElementById("a-download").addEventListener('click', function(){
     this.href = document.getElementById("canvasID").toDataURL();
-    this.download = "canvas-image.png";
+    this.download = `${score}%.png`;
 });
+
+const autoScreen = (delay) => {
+    setTimeout(() => {document.getElementById("a-make").click()}, delay);
+    setTimeout(() => {document.getElementById("a-download").click()}, delay*2);
+    setTimeout(() => {window.open(window.location.href.replace(`?&score=${score}`, `?&score=${parseInt(score) + 1}`), '_self')}, delay*4);
+}
+
+// Odkomentuj jak chcesz, żeby generowało screeny automatycznie :)
+// Testowane na Firefoxie, Chrome blokuje pobieranie mimo wyłączonych opcji bezpieczeństwa nie wiem czemu
+// if(score < 100){
+//     autoScreen(2000);
+// }
