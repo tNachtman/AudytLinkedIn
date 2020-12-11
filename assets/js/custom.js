a31b4b886a36673e0e56f876b47b755f3beab651
Tymoteusz Nachtman <tynach99@gmail.com>
2020-12-11 16:52:40 +0100

    Dynamic og:image, change filetype to PHP

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,33 +23,6 @@ const get_score_text = (score) => {
     }
 }
 
-let test = "";
-const makeScreenshot = () => {
-    html2canvas(document.getElementById("screenshot"), {scale: 1, allowTaint: true, useCORS: true}).then(canvas => {
-        canvas.id = "canvasID";
-        let main = document.getElementById("main");
-        while (main.firstChild){ main.removeChild(main.firstChild);}
-        main.appendChild(canvas);
-    });
-}
-
 $('#diagram').attr('data-circles-value', score);
 $('#punktacja').text(score + " / 100 pkt.");
 $('#opis').text(get_score_text(score));
-$('#a-make').on('click', makeScreenshot);
-document.getElementById("a-download").addEventListener('click', function(){
-    this.href = document.getElementById("canvasID").toDataURL();
-    this.download = `${score}%.png`;
-});
-
-const autoScreen = (delay) => {
-    setTimeout(() => {document.getElementById("a-make").click()}, delay);
-    setTimeout(() => {document.getElementById("a-download").click()}, delay*2);
-    setTimeout(() => {window.open(window.location.href.replace(`?&score=${score}`, `?&score=${parseInt(score) + 1}`), '_self')}, delay*4);
-}
-
-// Odkomentuj jak chcesz, żeby generowało screeny automatycznie :)
-// Testowane na Firefoxie, Chrome blokuje pobieranie mimo wyłączonych opcji bezpieczeństwa nie wiem czemu
-// if(score < 100){
-//     autoScreen(2000);
-// }
